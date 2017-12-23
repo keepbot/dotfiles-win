73372735fe13fec8169e856075ec1c2740d81445
Dmitriy Ivanov <divanov@aligntech.com>
2017-12-24 01:15:06 +0300

    refactor(setup): stabilized install script

--- install.ps1
+++ install.ps1
@@ -6,8 +6,8 @@ $dotfilesProfileDir = Join-Path $PSScriptRoot "powershell"
 "DEVELOPMENT" | Out-File ( Join-Path $PSScriptRoot "bash/var.env"    )
 "COMPLEX"     | Out-File ( Join-Path $PSScriptRoot "bash/var.prompt" )
 
-# Initial - Dirty SO Dirty
-#Remove-Item -Force -Confirm:$false -Recurse $profileDir
+# Making Symlinks
+# Remove-Item -Force -Confirm:$false -Recurse $profileDir
 If (Test-Path $profileDir                      )  {[System.IO.Directory]::Delete(                $profileDir              , $true)}
 If (Test-Path (Join-Path $HOME ".bash"        ))  {[System.IO.Directory]::Delete(              ( Join-Path $HOME ".bash" ), $true)}
 If (Test-Path (Join-Path $HOME ".bin"         ))  {[System.IO.Directory]::Delete(              ( Join-Path $HOME ".bin"  ), $true)}
@@ -25,19 +25,22 @@ If (Test-Path (Join-Path $HOME ".vimrc"       ))  {Remove-Item -Force -Confirm:$
 
 cmd /c mklink /d $profileDir $dotfilesProfileDir
 cmd /c mklink /d ( Join-Path $HOME ".bash"         ) ( Join-Path $PSScriptRoot "bash"         )
+cmd /c mklink /d ( Join-Path $HOME ".bin"          ) ( Join-Path $PSScriptRoot "bin-win"      )
+cmd /c mklink /d ( Join-Path $HOME ".tmux"         ) ( Join-Path $PSScriptRoot "tmux"         )
+cmd /c mklink /d ( Join-Path $HOME ".vim"          ) ( Join-Path $PSScriptRoot "vim"          )
+
 cmd /c mklink    ( Join-Path $HOME ".bash_profile" ) ( Join-Path $PSScriptRoot "bash_profile" )
 cmd /c mklink    ( Join-Path $HOME ".profile"      ) ( Join-Path $PSScriptRoot "bash_profile" )
-cmd /c mklink /d ( Join-Path $HOME ".bin"          ) ( Join-Path $PSScriptRoot "bin-win"      )
 cmd /c mklink    ( Join-Path $HOME ".bashrc"       ) ( Join-Path $PSScriptRoot "bashrc"       )
 cmd /c mklink    ( Join-Path $HOME ".gemrc"        ) ( Join-Path $PSScriptRoot "gemrc"        )
 cmd /c mklink    ( Join-Path $HOME ".gitconfig"    ) ( Join-Path $PSScriptRoot ".gitconfig"   )
 cmd /c mklink    ( Join-Path $HOME ".gitmessage"   ) ( Join-Path $PSScriptRoot ".gitmessage"  )
-cmd /c mklink /d ( Join-Path $HOME ".tmux"         ) ( Join-Path $PSScriptRoot "tmux"         )
 cmd /c mklink    ( Join-Path $HOME ".tmux.conf"    ) ( Join-Path $PSScriptRoot "tmux.conf"    )
-cmd /c mklink /d ( Join-Path $HOME ".vim"          ) ( Join-Path $PSScriptRoot "vim"          )
 cmd /c mklink    ( Join-Path $HOME ".vimrc"        ) ( Join-Path $PSScriptRoot "vimrc-win"    )
 
-
+# Cleanup
+Remove-Variable $profileDir
+Remove-Variable $dotfilesProfileDir
 
 #Write-Host "mklink $profileDir ./powershell"
 #cmd /c mklink c:\path\to\symlink c:\target\file
