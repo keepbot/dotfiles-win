31eb2619b08fbe5c026bb6218f044606a9d35f8b
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-05-11 12:23:18 +0300

    :heavy_plus_sign: add(powershell): Functions to manage Store apps + ConEmu++

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -212,3 +212,24 @@ function Unzip-File {
     }
 }
 
+function rm-wapp {
+  if ($args.Count -ne 1) {
+    Write-Host "Usage: rm-wapp <package_mask>"
+  }
+  else {
+    Get-AppxPackage *$($args[0].ToString())* | Remove-AppxPackage
+  }
+}
+
+function get-wapp {
+  if ($args.Count -ne 1) {
+    Write-Host "Usage: get-wapp <package_mask>"
+  }
+  else {
+    Get-AppxPackage *$($args[0].ToString())*
+  }
+}
+
+function re-wapp {
+  Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+}
