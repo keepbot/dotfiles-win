18b3e748962e94059799f46ba9e569c536eaa4c9
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-05-23 11:34:33 +0300

    fix: time function for powershell

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -1,4 +1,4 @@
-#!/usr/bin/env powershell
+#!/usr/bin/env pwsh
 # Easier Navigation: .., ..., ...., ....., and ~
 ${function:~} = { Set-Location ~ }
 # PoSh won't allow ${function:..} because of an invalid path error, so...
@@ -19,7 +19,37 @@ ${function:ws-df} = { Set-Location ~\workspace\my\dotfiles }
 ${function:ws-tmp} = { Set-Location ~\workspace\tmp }
 
 # Missing Bash aliases
-Set-Alias time Measure-Command
+# Set-Alias time Measure-Command
+# ${function:time} = { Measure-Command { @args }}
+${function:time} = {
+  $command = $args
+  $timings = $(Measure-Command {Invoke-Expression "${command}" | Out-Default})
+  $obj = New-Object PSObject
+  $obj | Add-Member Ticks $timings.Ticks
+  $obj | Add-Member Hours $timings.TotalHours
+  $obj | Add-Member Minutes $timings.TotalMinutes
+  $obj | Add-Member Seconds $timings.TotalSeconds
+  $obj | Add-Member Milliseconds $timings.TotalMilliseconds
+  # $str = "| " + $obj + " |"
+  # Write-Host $('-' * $str.Length)
+  # Write-Host $str
+  # Write-Host $('-' * $str.Length)
+  Write-Host
+  # Write-Host $('-' * 17) " Time report " $('-' * 18)
+  # Write-Host $('-' * 50)
+  Write-Host "Ticks       :" $timings.Ticks
+  Write-Host "Hours       :" $timings.TotalHours
+  Write-Host "Minutes     :" $timings.TotalMinutes
+  Write-Host "Seconds     :" $timings.TotalSeconds
+  Write-Host "Miliseconds :" $timings.TotalMilliseconds
+  # Write-Host $('-' * 50)
+}
+# ${function:time} = {
+#   $sw = [Diagnostics.Stopwatch]::StartNew()
+#   Invoke-Expression "${args}" | Out-Default
+#   $sw.Stop()
+#   $sw.Elapsed
+# }
 
 # Create a new directory and enter it
 Set-Alias mkd CreateAndSet-Directory
