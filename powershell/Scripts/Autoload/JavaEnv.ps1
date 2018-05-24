0a91be37866bf379dfb9021e05391be667118ba1
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-05-24 18:24:37 +0300

    add(powershell): Java Environment
    Functions to controk Java environmnet in current console session.
      - List-Java <Arch(x86 or x64)> - to list installed versions
      - Use-Java <JavaVersion> <Arch(x86 or x64)> - to use Java from
        previous list.

---
+++ powershell/Scripts/Autoload/JavaEnv.ps1
@@ -0,0 +1,118 @@
+#!/usr/env/pwsh
+function Use-Java {
+  <#
+  .SYNOPSIS
+    Enable to use particular version of JAVA within current session.
+
+  .DESCRIPTION
+    Enable to use particular version of JAVA within current session.
+
+  .PARAMETER JavaVersion
+    Version of jdk.
+
+  .PARAMETER Arch
+    Processor architecture x86 or X64(default).
+
+  .EXAMPLE
+    Use-Java jdk-10.0.1
+
+  .INPUTS
+    String
+
+  .OUTPUTS
+    None
+
+  .NOTES
+    Written by: Dmitriy Ivanov
+  #>
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory=$true)]
+      [string]$JavaVersion,
+      [ValidateNotNullOrEmpty()]
+      [string]$Arch = "x64"
+  )
+
+  switch ($Arch) {
+    "x64" {
+      $Versions = $((Get-ChildItem 'C:\Program Files\Java\').Name)
+      if ($Versions.Contains(${JavaVersion})) {
+        $env:JAVA_HOME = "C:\Program Files\Java\${JavaVersion}"
+        $env:PATH = "${env:JAVA_HOME}\bin;${env:PATH}"
+      }
+      else {
+        Write-Host "JAVA `"${JavaVersion}`" does not exist. Please use one of following:"
+        foreach($v in $Versions){
+          Write-Host " -" $v
+        }
+      }
+      break
+    }
+    "x86" {
+      $Versions = $((Get-ChildItem 'C:\Program Files (x86)\Java\').Name)
+      if ($Versions.Contains(${JavaVersion})) {
+        $env:JAVA_HOME = "C:\Program Files ($Arch)\Java\${JavaVersion}"
+        $env:PATH = "${env:JAVA_HOME}\bin;${env:PATH}"
+      }
+      else {
+        Write-Host "JAVA `"${JavaVersion}`" does not exist. Please use one of following:"
+        foreach($v in $Versions){
+          Write-Host " -" $v
+        }
+      }
+      break
+    }
+    default {
+      Write-Error -Message "ERROR: Wrong processor architecture!"
+      break
+    }
+ }
+}
+
+function List-Java {
+  <#
+  .SYNOPSIS
+    List Java version on current PC.
+
+  .DESCRIPTION
+    List Java version on current PC.
+
+  .PARAMETER Arch
+    Processor architecture x86 or X64(default).
+
+  .EXAMPLE
+    List-Java x64
+
+  .INPUTS
+    String
+
+  .OUTPUTS
+    String Array
+
+  .NOTES
+    Written by: Dmitriy Ivanov
+  #>
+  [CmdletBinding()]
+  param (
+      [ValidateNotNullOrEmpty()]
+      [string]$Arch = "x64"
+  )
+
+  switch ($Arch) {
+    "x64" {
+      $Versions = $((Get-ChildItem 'C:\Program Files\Java\').Name)
+      break
+    }
+    "x86" {
+      $Versions = $((Get-ChildItem 'C:\Program Files (x86)\Java\').Name)
+      break
+    }
+    default {
+      Write-Error -Message "ERROR: Wrong processor architecture!"
+      break
+    }
+  }
+
+    Write-Host "List of JAVA installef for ${Arch} on this PC:"
+    foreach($v in $Versions) {Write-Host " -" $v}
+}
