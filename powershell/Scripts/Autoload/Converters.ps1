b3d2f5547b0afdef8a39128d3d957b0a25a5d701
Dmitriy Ivanov <d.k.ivanov@live.com>
2018-10-30 03:12:19 +0300

    posershell pipline base64 converter

---
+++ powershell/Scripts/Autoload/Converters.ps1
@@ -0,0 +1,23 @@
+function ConvertTo-Base64() {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string] $string
+  )
+  $bytes  = [System.Text.Encoding]::UTF8.GetBytes($string);
+  $encoded = [System.Convert]::ToBase64String($bytes);
+
+  return $encoded;
+}
+
+function ConvertFrom-Base64() {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string] $string
+  )
+  $bytes  = [System.Convert]::FromBase64String($string);
+  $decoded = [System.Text.Encoding]::UTF8.GetString($bytes);
+
+  return $decoded;
+}
