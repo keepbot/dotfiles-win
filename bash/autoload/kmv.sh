bb561cd789ed85741aa3cdecfb7435c4770b177f
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-03-08 21:48:57 +0300

    add fincton to list iommu groups

--- bash/autoload/kmv.sh
+++ bash/autoload/kmv.sh
@@ -1,4 +1,4 @@
-check_iommu() {
+iommu-list1() {
     if [[ `dmesg | grep 'IOMMU enabled'` ]]; then
     echo "IOMMU is enabled. List of devices: "
         for d in /sys/kernel/iommu_groups/*/devices/*; do
@@ -6,6 +6,20 @@ check_iommu() {
             n=${n%%/*}
             printf 'IOMMU Group %s ' "$n"
             lspci -nns "${d##*/}"
+        done
+    else
+        print_error "ERROR: IOMMU is disabled"
+    fi
+}
+
+iommu-list2() {
+    if [[ `dmesg | grep 'IOMMU enabled'` ]]; then
+        for iommu_group in $(find /sys/kernel/iommu_groups/ -maxdepth 1 -mindepth 1 -type d); do
+            echo "IOMMU group $(basename "$iommu_group")"
+            for device in $(ls -1 "$iommu_group"/devices/); do
+                echo -n $'\t'
+                lspci -nns "$device"
+            done
         done
     else
         print_error "ERROR: IOMMU is disabled"
