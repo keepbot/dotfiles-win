741e784382eb48fdfab480e4ce172e0380fc56a4
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-03-08 23:59:21 +0300

    add vfio binding function

--- bash/autoload/kmv.sh
+++ bash/autoload/kmv.sh
@@ -25,3 +25,15 @@ iommu-list2() {
         print_error "ERROR: IOMMU is disabled"
     fi
 }
+
+vfio-bind() {
+    modprobe vfio-pci
+    for dev in "$@"; do
+        vendor=$(cat /sys/bus/pci/devices/$dev/vendor)
+        device=$(cat /sys/bus/pci/devices/$dev/device)
+        if [ -e /sys/bus/pci/devices/$dev/driver ]; then
+            echo $dev > /sys/bus/pci/devices/$dev/driver/unbind
+        fi
+        echo $vendor $device > /sys/bus/pci/drivers/vfio-pci/new_id
+    done
+}
