4023ab83ff235c35bdc8da83f3c7b1d48cbbf776
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-03-13 21:22:26 +0300

    reduce dpi and add couple display functions

--- bin-nix/set-display.sh
+++ bin-nix/set-display.sh
@@ -102,17 +102,68 @@ case $1 in
         nvidia-settings --assign \
             CurrentMetaMode="DPY-5: nvidia-auto-select @3840x2160 +0+0 {ViewPortIn=3840x2160, ViewPortOut=3840x2160+0+0}, DPY-2: nvidia-auto-select @3840x2160 +3840+0 {ViewPortIn=3840x2160, ViewPortOut=1920x1080+0+0, ForceFullCompositionPipeline=On}"
         ;;
+    dock-reset )
+        xrandr                                                                 \
+         --output eDP1      --auto --panning 3840x2160+0+0                     \
+         --output DP2-2     --off                                              \
+         --output DP2-3     --off                                              \
+         --output DP1       --off                                              \
+         --output DP2       --off                                              \
+         --output DP2-1     --off                                              \
+         --output DP3       --off                                              \
+         --output HDMI1     --off                                              \
+         --output HDMI2     --off                                              \
+         --output HDMI3     --off                                              \
+         --output VIRTUAL1  --off
+        ;;
     dock )
-        xrandr --fb 7680x4320                                               \
-         --output eDP1   --mode 3840x2160                                   \
-         --pos 1920x2160 --rotate normal                                    \
-         --primary                          --panning 3840x2160+1920+2160   \
-         --output DP2-2  --mode 1920x1080                                   \
-         --pos 0x0       --rotate normal                                    \
-         --scale 2x2     --above eDP1       --panning 3840x2160+0+0         \
-         --output DP2-3  --mode 1920x1080                                   \
-         --pos 3840x0    --rotate normal                                    \
-         --scale 2x2     --right-of DP2-2   --panning 3840x2160+3840+0
+        xrandr                                          \
+         --output eDP1   --mode 3840x2160               \
+         --pos 0x2160    --rotate normal --primary      \
+         --output DP2-2  --mode 1920x1080               \
+         --pos 0x0       --rotate normal --above eDP1   \
+         --output DP2-3  --mode 1920x1080               \
+         --pos 1920x0    --rotate normal --above eDP1
+        ;;
+    dock2 )
+        # xrandr              --fb 11520x2160                                    \
+        xrandr                                                                 \
+         --output DP2-2     --mode 1920x1080  --crtc 1                         \
+         --pos 0x0          --rotate normal                                    \
+         --scale 2x2        --left-of eDP1    --panning 3840x2160+0+0          \
+         --output eDP1      --mode 3840x2160  --crtc 0                         \
+         --pos 3840x0       --rotate normal                                    \
+         --primary                            --panning 3840x2160+3840+0       \
+         --output DP2-3     --mode 1920x1080  --crtc 2                         \
+         --pos 7680x0       --rotate normal                                    \
+         --scale 2x2        --right-of eDP1   --panning 3840x2160+7680+0       \
+         --output DP1       --off                                              \
+         --output DP2       --off                                              \
+         --output DP2-1     --off                                              \
+         --output DP3       --off                                              \
+         --output HDMI1     --off                                              \
+         --output HDMI2     --off                                              \
+         --output HDMI3     --off                                              \
+         --output VIRTUAL1  --off
+        ;;
+    dock3 )
+        # xrandr              --fb 11520x2160                                    \
+        xrandr                                                                 \
+         --output DP2-2     --mode 1920x1080  --crtc 1                         \
+         --pos 0x0          --rotate normal                                    \
+         --scale 2x2        --left-of eDP1    --panning 3840x2160+0+0          \
+         --output eDP1      --mode 3840x2160  --crtc 0                         \
+         --pos 3840x0       --rotate normal                                    \
+         --primary                            --panning 3840x2160+3840+0       \
+         --output DP2-3     --off                                              \
+         --output DP1       --off                                              \
+         --output DP2       --off                                              \
+         --output DP2-1     --off                                              \
+         --output DP3       --off                                              \
+         --output HDMI1     --off                                              \
+         --output HDMI2     --off                                              \
+         --output HDMI3     --off                                              \
+         --output VIRTUAL1  --off
         ;;
     * )
         echo "Unknown display configuration"
