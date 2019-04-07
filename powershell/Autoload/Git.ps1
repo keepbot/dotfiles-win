627da399620b66084d203252b8fd6ec8671f4d00
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-04-07 20:34:01 +0300

    add powershell function to clone GH repos by ssh

--- powershell/Autoload/Git.ps1
+++ powershell/Autoload/Git.ps1
@@ -91,11 +91,20 @@ if (Get-Command git.exe -ErrorAction SilentlyContinue | Test-Path) {
   }
 
   # GitHub
-  ${function:get_gh_user_repos} = {
+  ${function:get_gh_user_repos_https} = {
     Write-Host "Clonning all GH repos of $($args[0])"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?per_page=1000" | ConvertFrom-Json
     foreach( $repo in $repoList.clone_url){
+      git.exe clone $repo
+    }
+  }
+
+  ${function:get_gh_user_repos_ssh} = {
+    Write-Host "Clonning all GH repos of $($args[0])"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?per_page=1000" | ConvertFrom-Json
+    foreach( $repo in $repoList.ssh_url){
       git.exe clone $repo
     }
   }
