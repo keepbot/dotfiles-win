e3ccbcdf80a5046f2b4cb3ca2cde0062eb20ea3a
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-06-01 04:13:05 +0300

    add initial grep implementation for powershell

--- powershell/Autoload/Search.ps1
+++ powershell/Autoload/Search.ps1
@@ -1,7 +1,72 @@
 # Greps with status
 if (Get-Command busybox.exe -ErrorAction SilentlyContinue | Test-Path) {
-    ${function:grep} = { busybox.exe grep @args }
-    ${function:gerp} = { grep @args }
+    ${function:grep} = {
+        [CmdletBinding()]
+        Param (
+            [Parameter(Mandatory=$true)]
+            [string]$Pattern,
+            [switch]$c,
+            [switch]$E,
+            [switch]$F,
+            [switch]$hh,
+            [switch]$H,
+            [switch]$i,
+            [switch]$ll,
+            [switch]$L,
+            [switch]$n,
+            [switch]$o,
+            [switch]$q,
+            [switch]$r,
+            [switch]$s,
+            [switch]$v,
+            [switch]$w,
+            [switch]$x,
+            [Parameter(ValueFromPipeline=$true)]
+            $Data = $null,
+            [string]$Other = $null
+        )
+        Begin {
+            [string] $SessionID = [System.Guid]::NewGuid()
+            [string] $TempFile  = (Join-Path $env:Temp $SessionID'.grep')
+        }
+        Process {
+            if (-Not $Data) { Write-Host "ERROR: File not defined"; return }
+            $File = ""
+            if (Test-Path $Data) {
+                $File = $Data
+            } else {
+                Add-Content "$TempFile" $Data
+                $File = $TempFile
+            }
+            $Arguments = "-e $Pattern"
+            if ($c)         {$Arguments += " -c"}
+            if ($E)         {$Arguments += " -E"}
+            if ($F)         {$Arguments += " -F"}
+            if ($hh)        {$Arguments += " -h"}
+            if ($H)         {$Arguments += " -H"}
+            if ($i)         {$Arguments += " -i"}
+            if ($ll)        {$Arguments += " -l"}
+            if ($L)         {$Arguments += " -L"}
+            if ($n)         {$Arguments += " -n"}
+            if ($o)         {$Arguments += " -o"}
+            if ($q)         {$Arguments += " -q"}
+            if ($r)         {$Arguments += " -r"}
+            if ($s)         {$Arguments += " -s"}
+            if ($v)         {$Arguments += " -v"}
+            if ($w)         {$Arguments += " -w"}
+            if ($x)         {$Arguments += " -x"}
+            if ($Other)     {$Arguments += " $Other"}
+
+            Invoke-Expression "busybox.exe grep $Arguments $File"
+        }
+
+        End {
+            Remove-Item -Force -ErrorAction SilentlyContinue "$TempFile"
+
+        }
+    }
+    Set-Alias -Name gerp -Value grep
+    Set-Alias -Name greo -Value grep
     ${function:gHS}  = { grep -e "status" -e "health" @args }
 } else {
     Set-Alias -Name grep -Value Select-String
