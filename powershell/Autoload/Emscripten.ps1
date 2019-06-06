2378fa338a8fe5a943e1324b2942d839dd47afce
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-06-06 12:34:44 +0300

    fix emscripten environmet for windows

--- powershell/Autoload/Emscripten.ps1
+++ powershell/Autoload/Emscripten.ps1
@@ -1,7 +1,7 @@
 if (Get-Command emsdk -ErrorAction SilentlyContinue | Test-Path) {
-    ${function:ema} = { if ($args) {emsdk activate @args} else {emsdk activate latest}}
-    ${function:emi} = { if ($args) {emsdk install  @args} else {emsdk install  latest}}
-    ${function:eml} = { emsdk list }
-    ${function:emup} = { emsdk update }
-    ${function:emun} = { emsdk uninstall @args }
+    ${function:ema} = { if ($args) {emsdk.ps1 activate @args} else {emsdk.ps1 activate latest}}
+    ${function:emi} = { if ($args) {emsdk.ps1 install  @args} else {emsdk.ps1 install  latest}}
+    ${function:eml} = { emsdk.ps1 list }
+    ${function:emup} = { emsdk.ps1 update }
+    ${function:emun} = { emsdk.ps1 uninstall @args }
 }
