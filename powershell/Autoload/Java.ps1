fe460c2c2dab84f3b2fbdae6899ab9761d7e854a
Dmitriy Ivanov <d.k.ivanov@live.com>
2019-11-06 11:05:58 +0300

    refactoting java-related windows function

--- powershell/Autoload/Java.ps1
+++ powershell/Autoload/Java.ps1
@@ -1,105 +1,74 @@
-#!/usr/env/pwsh
+function Get-JavaList {
+    <#
+    .SYNOPSIS
+        List installed Java versions on current PC.
+    .DESCRIPTION
+        List installed Java versions on current PC.
+    .EXAMPLE
+        Get-JavaList
+    .INPUTS
+        None
+    .OUTPUTS
+        Validated Java Kits Array
+    .NOTES
+        Written by: Dmitriy Ivanov
+    #>
+
+    $javas = @()
+    $((Get-ChildItem 'C:\Program Files\Java\').FullName         | ForEach-Object { $javas += $_ })
+    $((Get-ChildItem 'C:\Program Files (x86)\Java\').FullName   | ForEach-Object { $javas += $_ })
+
+    $javasValidated = @()
+    foreach($java in $javas) {
+        if (Test-Path $java) {
+            $javasValidated  += $java
+        }
+    }
+
+    return $javasValidated
+}
+
+function Find-Java {
+    <#
+    .SYNOPSIS
+        List installed Java versions on current PC.
+    .DESCRIPTION
+        List installed Java versions on current PC.
+    .EXAMPLE
+        Find-Java
+    .INPUTS
+        None
+    .OUTPUTS
+        Java Kits Array
+    .NOTES
+        Written by: Dmitriy Ivanov
+    #>
+
+    Write-Host "List of Java Kits on this PC:"
+    $javas = @()
+    $((Get-ChildItem 'C:\Program Files\Java\').Name         | ForEach-Object { $javas += "x64 -> $_" })
+    $((Get-ChildItem 'C:\Program Files (x86)\Java\').Name   | ForEach-Object { $javas += "x86 -> $_" })
+    return $javas
+}
+
+
 function Set-Java {
     <#
     .SYNOPSIS
         Enable to use particular version of JAVA within current session.
     .DESCRIPTION
         Enable to use particular version of JAVA within current session.
-    .PARAMETER JavaVersion
-        Version of jdk.
-    .PARAMETER Arch
-        Processor architecture x86 or X64(default).
     .EXAMPLE
-        Set-Java jdk-10.0.1
+        Set-Java
     .INPUTS
-        String
+        Note
     .OUTPUTS
         None
     .NOTES
         Written by: Dmitriy Ivanov
     #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$JavaVersion,
-        [ValidateNotNullOrEmpty()]
-        [string]$Arch = "x64"
-    )
-
-    switch ($Arch) {
-        "x64" {
-            $Versions = $((Get-ChildItem 'C:\Program Files\Java\').Name)
-            if ($Versions.Contains(${JavaVersion})) {
-                [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\${JavaVersion}", "Machine")
-                $env:JAVA_HOME = "C:\Program Files\Java\${JavaVersion}"
-                $env:PATH = "${env:JAVA_HOME}\bin;${env:PATH}"
-            }
-            else {
-                Write-Host "JAVA `"${JavaVersion}`" does not exist. Please use one of following:"
-                foreach($v in $Versions) {
-                    Write-Host " -" $v
-                }
-            }
-            break
-        }
-        "x86" {
-            $Versions = $((Get-ChildItem 'C:\Program Files (x86)\Java\').Name)
-            if ($Versions.Contains(${JavaVersion})) {
-                [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files ($Arch)\Java\${JavaVersion}", "Machine")
-                $env:JAVA_HOME = "C:\Program Files ($Arch)\Java\${JavaVersion}"
-                $env:PATH = "${env:JAVA_HOME}\bin;${env:PATH}"
-            }
-            else {
-                Write-Host "JAVA `"${JavaVersion}`" does not exist. Please use one of following:"
-                foreach($v in $Versions){
-                    Write-Host " -" $v
-                }
-            }
-            break
-        }
-        default {
-            Write-Error -Message "ERROR: Wrong processor architecture!"
-            break
-        }
-    }
-}
-
-function Find-Java {
-    <#
-    .SYNOPSIS
-        Find Java version on current PC.
-    .DESCRIPTION
-        Find Java version on current PC.
-    .PARAMETER Arch
-        Processor architecture x86 or X64(default).
-    .EXAMPLE
-        Find-Java
-    .INPUTS
-        String
-    .OUTPUTS
-        String Array
-    .NOTES
-        Written by: Dmitriy Ivanov
-    #>
-    [CmdletBinding()]
-    param (
-        [ValidateNotNullOrEmpty()]
-        [string]$Arch = "x64"
-    )
-    switch ($Arch) {
-        "x64" {
-            $Versions = $((Get-ChildItem 'C:\Program Files\Java\').Name)
-            break
-        }
-        "x86" {
-            $Versions = $((Get-ChildItem 'C:\Program Files (x86)\Java\').Name)
-            break
-        }
-        default {
-            Write-Error -Message "ERROR: Wrong processor architecture!"
-            break
-        }
-    }
-    Write-Host "List of JAVA installed for ${Arch} on this PC:"
-    foreach($v in $Versions) {Write-Host " -" $v}
+    $ChoosenJavaVersion = Select-From-List $(Get-JavaList) "Java path"
+    [Environment]::SetEnvironmentVariable("JAVA_HOME", ${ChoosenJavaVersion}, "Machine")
+    $env:JAVA_HOME = ${ChoosenJavaVersion}
+    $env:PATH = "${env:JAVA_HOME}\bin;${env:PATH}"
 }
