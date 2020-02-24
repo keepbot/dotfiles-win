bd65fab73e03b35089ad46f98b224ef2bf8f9a56
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-02-24 23:53:18 +0300

    chage loop in script to fetch all braches

--- bin-nix/git-pull-all-branches.sh
+++ bin-nix/git-pull-all-branches.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+# git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+for remote in `git branch -r`; do git branch --track ${remote#origin/} $remote; done
 git fetch --all
 git pull --all
