c6d56b4af17e79f3e45a95a62c2e91335e41e100
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-03-15 13:01:41 +0300

    extend function for clonning all gh user's repos

--- powershell/Autoload/GitHub.ps1
+++ powershell/Autoload/GitHub.ps1
@@ -1,32 +1,67 @@
 if (Get-Command git.exe -ErrorAction SilentlyContinue | Test-Path) {
+    # FIXME
     ${function:get_gh_user_repos_https} = {
-        Write-Host "Clonning all GH repos of $($args[0])"
+        Write-Host "Clonning source GH repos of $($args[0])"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000" | ConvertFrom-Json
-        foreach($repo in $repoList){
-            if(-Not $repo.fork) {
-                git.exe clone $repo.clone_url
+        for ($i = 1; $i -lt 10; $i++) {
+            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000&page=${i}" | ConvertFrom-Json
+            foreach($repo in $repoList){
+                if(-Not $repo.fork) {
+                    git.exe clone $repo.clone_url
+                }
             }
         }
     }
 
+    # FIXME
     ${function:get_gh_user_repos_ssh} = {
-        Write-Host "Clonning all GH repos of $($args[0])"
+        Write-Host "Clonning source GH repos of $($args[0])"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000" | ConvertFrom-Json
-        foreach($repo in $repoList){
-            if(-Not $repo.fork) {
-                git.exe clone $repo.ssh_url
+        for ($i = 1; $i -lt 10; $i++) {
+            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=100&page=${i}" | ConvertFrom-Json
+            foreach($repo in $repoList){
+                if(-Not $repo.fork) {
+                    git.exe clone $repo.ssh_url
+                }
             }
         }
     }
 
+    # FIXME
+    ${function:show_gh_user_repos_ssh} = {
+        Write-Host "Showing source GH repos of $($args[0])"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        for ($i = 1; $i -lt 10; $i++) {
+            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=100&page=${i}" | ConvertFrom-Json
+            foreach($repo in $repoList){
+                if(-Not $repo.fork) {
+                    Write-Output $repo.ssh_url
+                }
+            }
+        }
+    }
+
+    # FIXME
     ${function:get_gh_user_repos_ssh_all} = {
         Write-Host "Clonning all GH repos of $($args[0])"
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000" | ConvertFrom-Json
-        foreach($repo in $repoList.ssh_url){
-            git.exe clone $repo
+            for ($i = 1; $i -lt 10; $i++) {
+            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000" | ConvertFrom-Json
+            foreach($repo in $repoList.ssh_url){
+                git.exe clone $repo
+            }
+        }
+    }
+
+    # FIXME
+    ${function:show_gh_user_repos_ssh_all} = {
+        Write-Host "Showing all GH repos of $($args[0])"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            for ($i = 1; $i -lt 10; $i++) {
+            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000" | ConvertFrom-Json
+            foreach($repo in $repoList.ssh_url){
+                Write-Output $repo
+            }
         }
     }
 
