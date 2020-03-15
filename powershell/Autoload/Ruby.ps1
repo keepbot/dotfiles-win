4369cba5d60e69aee2e3a1f3665d064a36e27c26
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-03-15 19:28:32 +0300

    add alias to run ruby with all warnings turned on

--- powershell/Autoload/Ruby.ps1
+++ powershell/Autoload/Ruby.ps1
@@ -1,14 +1,17 @@
 # Ruby aliases
 if (Get-Command ruby.exe -ErrorAction SilentlyContinue | Test-Path) {
-    ${function:rre} = { ruby.exe exec @args }
+    ${function:ruby} = { ruby.exe -w @args }
+    ${function:rre}  = { ruby.exe exec @args }
 }
+
 if (Get-Command gem -ErrorAction SilentlyContinue | Test-Path) {
-    ${function:rgi} = { gem install @args }
-    ${function:rbi} = { gem bundle install @args }
+    ${function:rgi}  = { gem install @args }
+    ${function:rbi}  = { gem bundle install @args }
 }
+
 if (Get-Command bundle -ErrorAction SilentlyContinue | Test-Path) {
-    ${function:rbu} = { bundle update @args }
-    ${function:rbe} = { bundle exec @args }
+    ${function:rbu}  = { bundle update @args }
+    ${function:rbe}  = { bundle exec @args }
 }
 
 function Get-Rubies {
