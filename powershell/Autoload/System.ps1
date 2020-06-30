88a099f7d0b9120831e4da5860c6bf5b04e48f94
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-07-01 00:47:49 +0300

    add powershell functions to look for duplicates

--- powershell/Autoload/System.ps1
+++ powershell/Autoload/System.ps1
@@ -179,3 +179,33 @@ function Get-ConsoleCP
 {
     return ($(chcp) -replace '[^0-9]','')
 }
+
+function Get-DuplicatedModules {
+    Get-InstalledModule | ForEach-Object {
+        $latestVersion = $PSItem.Version
+
+        Write-Host "$($PSItem.Name) - $($PSItem.Version)" -ForegroundColor Green
+
+        Get-InstalledModule $PSItem.Name -AllVersions |
+        Where-Object Version -NE $latestVersion |
+        ForEach-Object {
+            Write-Host "- $($PSItem.Name) - $($PSItem.Version)" -ForegroundColor Magenta
+        }
+    }
+}
+
+function Uninstall-DuplicatedModules {
+    Get-InstalledModule | ForEach-Object {
+        $latestVersion = $PSItem.Version
+
+        Write-Host "$($PSItem.Name) - $($PSItem.Version)" -ForegroundColor Green
+
+        Get-InstalledModule $PSItem.Name -AllVersions |
+        Where-Object Version -NE $latestVersion |
+        ForEach-Object {
+            Write-Host "- Uninstalling version $($PSItem.Version)..." -ForegroundColor Magenta -NoNewline
+            $PSItem | Uninstall-Module -Force
+            Write-Host "done"
+        }
+    }
+}
