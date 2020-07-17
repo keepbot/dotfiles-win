149f5926de6b05d53cb6b856a0753f1477bc9c3d
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-07-17 03:36:51 +0300

    do not prompt use native version of visual studio code in WSL

--- bash/autoload/apps.sh
+++ bash/autoload/apps.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Do not prompt using Windows native with WSL
+export DONT_PROMPT_WSL_INSTALL='true'
+
 if command -v code-insiders >/dev/null; then
     alias icode="code-insiders"
 else
