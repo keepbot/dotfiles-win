d5e5101a76e5a4579bf7da676bbdbf116b3dbc67
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-07-26 15:21:24 +0300

    update qt to 5.15 directory structure

--- powershell/Autoload/QT.ps1
+++ powershell/Autoload/QT.ps1
@@ -17,30 +17,22 @@ if ($MyInvocation.InvocationName -ne '.')
 }
 
 
-function Find-QT {
-    <#
-    .SYNOPSIS
-        List QT Framework versions on current PC.
-    .DESCRIPTION
-        List QT Framework versions on current PC.
-    .EXAMPLE
-        Find-QT
-    .INPUTS
-        None
-    .OUTPUTS
-        None
-    .NOTES
-        Written by: Dmitriy Ivanov
-    #>
-    if (Test-Path "C:\Qt") {
-        $QTVersions = $(((Get-ChildItem "C:\Qt").Name) -replace '\D+(\d+.)','$1')
+function Find-QT
+{
+    if (Test-Path "C:\Qt")
+    {
+        $QTVersions = $(((Get-ChildItem "C:\Qt").Name) -match '\d+.\d+.\d+')
     }
+
     Write-Host "List of QT Toolset on this PC:"
-    if ($QTVersions) {
-        foreach($v in $QTVersions) {
-            $CurrentQT = "C:\Qt\Qt" + $v+ "\" + $v
-            (Get-ChildItem $CurrentQT).Name | ForEach-Object{
-                If ($_ -match "Src" -Or $_ -match "sha1s.txt"){
+    if ($QTVersions)
+    {
+        foreach($v in $QTVersions)
+        {
+            $CurrentQT = "C:\Qt\" + $v
+            (Get-ChildItem $CurrentQT).Name | ForEach-Object {
+                If ($_ -match "Src" -Or $_ -match "sha1s.txt")
+                {
                     continue
                 }
                 Write-Host " -"$v" -> "$_
@@ -49,35 +41,25 @@ function Find-QT {
     }
 }
 
-function Set-QT {
-    <#
-    .SYNOPSIS
-      Set QT Framework version on current PC.
-    .DESCRIPTION
-      Set QT Framework version on current PC.
-    .EXAMPLE
-      Set-QT
-    .INPUTS
-      None
-    .OUTPUTS
-      None
-    .NOTES
-      Written by: Dmitriy Ivanov
-    #>
-
-    if (Test-Path "C:\Qt") {
-      $QTVersions = $(((Get-ChildItem "C:\Qt").Name) -replace '\D+(\d+.)','$1')
-    } else {
+function Set-QT
+{
+    if (Test-Path "C:\Qt")
+    {
+        $QTVersions = $(((Get-ChildItem "C:\Qt").Name) -match '\d+.\d+.\d+')
+    }
+    else
+    {
         Write-Host "ERROR: QT doesn't installed to path 'C:\Qt'..." -ForegroundColor Red
         break
     }
 
     $ChoosenQTVersion = Select-From-List $QTVersions "QT Version"
 
-    $CurrentQTPath = "C:\Qt\Qt" + $ChoosenQTVersion + "\" + $ChoosenQTVersion
+    $CurrentQTPath = "C:\Qt\" + $ChoosenQTVersion
     $QTToolsets = @()
-    (Get-ChildItem $CurrentQTPath).Name | ForEach-Object{
-        If ($_ -Match "^Src$" -Or $_ -Match "^sha1s.txt$") {
+    (Get-ChildItem $CurrentQTPath).Name | ForEach-Object {
+        If ($_ -Match "^Src$" -Or $_ -Match "^sha1s.txt$")
+        {
             return
         }
         $QTToolsets += $_
@@ -90,43 +72,58 @@ function Set-QT {
     # Set-Env
 }
 
-function Set-QTShort {
+function Set-QTShort
+{
     [CmdletBinding()]
-    param (
+    param
+    (
         [ValidateNotNullOrEmpty()]
         [string]$Arch   = "x64"
     )
 
-    if ($env:QTDIR) {
+    if ($env:QTDIR)
+    {
         Remove-Item Env:QTDIR
     }
-    if ($env:QMAKESPEC) {
+
+    if ($env:QMAKESPEC)
+    {
         Remove-Item Env:QMAKESPEC
     }
-    if ($env:QMAKE_TARGET.arch) {
+
+    if ($env:QMAKE_TARGET.arch)
+    {
         Remove-Item Env:QMAKE_TARGET.arch
     }
 
-    if ($Arch -eq "x86"){
-        Set-Item -Path Env:QTDIR -Value "C:\Qt\Qt5.12.1\5.12.1\msvc2017"
-        Set-Item -Path Env:QMAKESPEC -Value "C:\Qt\Qt5.12.1\5.12.1\msvc2017\mkspecs\win32-msvc"
+    if ($Arch -eq "x86")
+    {
+        Set-Item -Path Env:QTDIR -Value "C:\Qt\5.15.0\msvc2019"
+        Set-Item -Path Env:QMAKESPEC -Value "C:\Qt\5.15.0\msvc2019\mkspecs\win32-msvc"
         Set-Item -Path Env:QMAKE_TARGET.arch -Value "x86"
-    } else {
-        Set-Item -Path Env:QTDIR -Value "C:\Qt\Qt5.12.1\5.12.1\msvc2017_64"
-        Set-Item -Path Env:QMAKESPEC -Value "C:\Qt\Qt5.12.1\5.12.1\msvc2017_64\mkspecs\win32-msvc"
+    }
+    else
+    {
+        Set-Item -Path Env:QTDIR -Value "C:\Qt\5.15.0\msvc2019_64"
+        Set-Item -Path Env:QMAKESPEC -Value "C:\Qt\5.15.0\msvc2019_64\mkspecs\win32-msvc"
         Set-Item -Path Env:QMAKE_TARGET.arch -Value "x64"
     }
 
     Set-Item -Path Env:PATH -Value "${Env:QTDIR}\bin;${Env:PATH}"
 }
 
-function Clear-QT {
+function Clear-QT
+{
     [Environment]::SetEnvironmentVariable("QTDIR", $null, "Machine")
     [Environment]::SetEnvironmentVariable("QMAKESPEC", $null, "Machine")
-    if ($env:QTDIR) {
+
+    if ($env:QTDIR)
+    {
         Remove-Item Env:QTDIR
     }
-    if ($env:QMAKESPEC) {
+
+    if ($env:QMAKESPEC)
+    {
         Remove-Item Env:QMAKESPEC
     }
     # Set-Env
