dc650d1afd239f67a895a0eb8c989c5ca1fda7be
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-08 18:21:24 +0300

    add cmake wrappers for generation and building

--- powershell/Autoload/CMake.ps1
+++ powershell/Autoload/CMake.ps1
@@ -24,4 +24,11 @@ if (Get-Command cmake.exe -ErrorAction SilentlyContinue | Test-Path) {
     ${function:cmake2017x64} = { mkdir build; Set-Location build; Set-VC-Vars-All x64; cmake -G "Visual Studio 15 2017" -A x64   -DCMAKE_BUILD_TYPE="Release" ..; cmake --build . --config "Release" }
     ${function:cmake2019x86} = { mkdir build; Set-Location build; Set-VC-Vars-All x86; cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_BUILD_TYPE="Release" ..; cmake --build . --config "Release" }
     ${function:cmake2019x64} = { mkdir build; Set-Location build; Set-VC-Vars-All x64; cmake -G "Visual Studio 16 2019" -A x64   -DCMAKE_BUILD_TYPE="Release" ..; cmake --build . --config "Release" }
+
+
+    ${function:cmake_gen_32} = { cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_BUILD_TYPE="Release" .. }
+    ${function:cmake_gen_64} = { cmake -G "Visual Studio 16 2019" -A x64   -DCMAKE_BUILD_TYPE="Release" .. }
+
+    ${function:cmake_build}  = { cmake --build . --config "Release" }
+
 }
