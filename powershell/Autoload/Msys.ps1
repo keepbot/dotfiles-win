8044dd613d868f43efeab980ec1572524ba8af41
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-10-11 02:19:09 +0300

    add wrapper to invoke GCC compilier from MinGW

--- powershell/Autoload/Msys.ps1
+++ powershell/Autoload/Msys.ps1
@@ -18,11 +18,18 @@ if ($MyInvocation.InvocationName -ne '.')
 
 
 function Use-Msys2 {
+    [CmdletBinding()]
+    param
+    (
+        [string] $arch = '64'
+    )
     $msys_path = $(Get-Command msys2.exe -ErrorAction SilentlyContinue | Split-Path)
 
     if ($msys_path) {
-        $msys_bin_path = $(Join-Path $msys_path "usr\bin")
-        Set-Item -Path Env:PATH -Value "$msys_bin_path;$Env:PATH"
+        $msys_bin_path  = $(Join-Path $msys_path "usr\bin")
+        $mingw_bin_path = $(Join-Path $msys_path "mingw${arch}\bin")
+
+        Set-Item -Path Env:PATH -Value "${msys_bin_path};${mingw_bin_path};${Env:PATH}"
     } else {
         Write-Host "ERROR: MSYS2 not found. Exiting..." -ForegroundColor Red
         return
