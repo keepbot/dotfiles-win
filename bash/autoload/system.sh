7ad6bd5ad7ac14ee7755d1d45e97ef5d4fced973
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-11-12 14:59:13 +0300

    add aliases to clean system cashes for linux

---
+++ bash/autoload/system.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+alias clear_mem_1='sudo bash -c "sync; echo 1 > /proc/sys/vm/drop_caches"'
+alias clear_mem_2='sudo bash -c "sync; echo 2 > /proc/sys/vm/drop_caches"'
+alias clear_mem_3='sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
