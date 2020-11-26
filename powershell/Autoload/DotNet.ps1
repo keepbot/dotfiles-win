5bf2d5ebc2282958b66103b816ead33ee4001785
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-11-27 00:10:30 +0300

    add powershell completion for dotnet cli

--- powershell/Autoload/DotNet.ps1
+++ powershell/Autoload/DotNet.ps1
@@ -99,3 +99,16 @@ function Get-DotNet-FrameworkVersion
         }
     }
 }
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param
+    (
+        $commandName,
+        $wordToComplete,
+        $cursorPosition
+    )
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
