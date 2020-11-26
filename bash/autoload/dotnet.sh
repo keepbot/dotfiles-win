97f80eb2614853d592f65ef95b544b80bb484531
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-11-27 00:06:36 +0300

    add bash completion for dotnet cli

--- bash/autoload/dotnet.sh
+++ bash/autoload/dotnet.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+# bash parameter completion for the dotnet CLI
+_dotnet_bash_complete()
+{
+    local word=${COMP_WORDS[COMP_CWORD]}
+
+    local completions
+    completions="$(dotnet complete --position "${COMP_POINT}" "${COMP_LINE}" 2>/dev/null)"
+    if [ $? -ne 0 ]; then
+        completions=""
+    fi
+
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+}
+complete -f -F _dotnet_bash_complete dotnet
