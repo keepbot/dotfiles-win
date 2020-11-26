7e03f2f66160034fb35ad0281f6cbe7d544ca2ae
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-11-27 00:20:53 +0300

    add dotnet 5.0 installation script for ubuntu 20.04

--- bash/autoload/dotnet.sh
+++ bash/autoload/dotnet.sh
@@ -16,3 +16,13 @@ _dotnet_bash_complete()
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
 }
 complete -f -F _dotnet_bash_complete dotnet
+
+install_dotnet_2004()
+{
+    wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+    sudo dpkg -i /tmp/packages-microsoft-prod.deb
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get update
+    sudo apt-get install -y dotnet-sdk-5.0
+}
