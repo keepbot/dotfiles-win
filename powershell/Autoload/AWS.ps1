172d13864be3a03dab0f4851d777518669fd418e
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-12-05 20:49:19 +0300

    add couple of AWS functions to describe instances

--- powershell/Autoload/AWS.ps1
+++ powershell/Autoload/AWS.ps1
@@ -32,3 +32,37 @@ function aws_set_env_vars()
     $Env:AWS_SECRET_ACCESS_KEY  = "$SecretKey"
     $Env:AWS_DEFAULT_REGION     = "$Region"
 }
+
+
+# Get all instances state
+function aws_print_all_instances()
+{
+    $REGIONS=$(aws ec2 describe-regions --region eu-central-1 --output text --query Regions[*].[RegionName])
+    foreach ($REGION in $REGIONS)
+    {
+        Write-Host "Instances in '$REGION'.." -ForegroundColor Green
+        # aws ec2 describe-instances --region $REGION | jq '.Reservations[].Instances[] | "EC2: \(.InstanceId): \(.State.Name)"'
+        $region_info = $(aws ec2 describe-instances --region $REGION | ConvertFrom-Json)
+        foreach ($instance in $region_info.Reservations.Instances)
+        {
+            Write-Host "$($instance.InstanceId)`t$($instance.State.Name)`t$($instance.InstanceType)" -ForegroundColor Yellow
+        }
+    }
+}
+
+# Get all instances public and private IPs
+function aws_print_all_ip()
+{
+    $REGIONS=$(aws ec2 describe-regions --region eu-central-1 --output text --query Regions[*].[RegionName])
+    foreach ($REGION in $REGIONS)
+    {
+        Write-Host "Instances in '$REGION'.." -ForegroundColor Green
+        # aws ec2 describe-instances --region $REGION | jq '.Reservations[].Instances[] | "EC2: \(.InstanceId): \(.State.Name)"'
+        $region_info = $(aws ec2 describe-instances --region $REGION | ConvertFrom-Json)
+        foreach ($instance in $region_info.Reservations.Instances)
+        {
+            Write-Host "$($instance.InstanceId)`t$($instance.PrivateIpAddress)" -ForegroundColor Yellow -NoNewline
+            Write-Host "`t$($instance.PublicIpAddress)" -ForegroundColor Red
+        }
+    }
+}
