615e1766b969d5df10599a2f9adca9f63bcbea9c
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-12-24 02:34:14 +0300

    add color 256 invocation to powershell profile

--- powershell/profile_loader.ps1
+++ powershell/profile_loader.ps1
@@ -26,6 +26,8 @@ If (Get-Command cmder.exe -ErrorAction SilentlyContinue | Test-Path) {
 
 #Set-Location "~/workspace/my/dotfiles/"
 
+# Invovoke ANSI 256 Color Console
+AnsiColors256
+
 # Write-Host "Welcome Home:"(Get-WmiObject -Class Win32_UserAccount -Filter "Name = '$env:USERNAME'").FullName
 Write-Host "Welcome Home: $(Split-Path (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object UserName).UserName -Leaf)"
-
