72ff38ea0653be03607c812233c56afba8a5f327
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-12-23 11:31:21 +0300

    add gpg aliases to bash environment

--- bash/autoload/crypto.sh
+++ bash/autoload/crypto.sh
@@ -4,5 +4,14 @@
 alias rot13="tr '[A-Za-z]' '[N-ZA-Mn-za-m]'"
 
 alias sha='shasum -a 256'
-alias ggg='gpg --dry-run -vvvv --import'
 alias genpass='openssl rand -base64'
+
+# GPG Aliases
+alias gpg_show_keys='gpg --list-secret-keys --keyid-format LONG'
+
+alias ggg='gpg --dry-run -vvvv --import'
+alias gpg_show_key_info='gpg --import-options show-only --import --fingerprint'
+
+alias gpg_search_ubuntu='gpg --keyserver keyserver.ubuntu.com --search-key'
+alias gpg_search_sks='gpg --keyserver pool.sks-keyservers.net --search-key'
+alias gpg_search_mit='gpg --keyserver pgp.mit.edu --search-key'
