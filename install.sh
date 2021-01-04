6f98c8e69bd108d61cd07e8eec0437ed702aeac8
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-01-04 16:24:54 +0300

    add loading powershell config on linux - just in case - a lot of errors

--- install.sh
+++ install.sh
@@ -5,39 +5,39 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Script
 case $1 in
-	# Cases for users that know what to do
-	prod)
-		echo "PRODUCTION"  > "$DOTFILES_DIR/bash/var.env"
-		echo "COMPLEX"     > "$DOTFILES_DIR/bash/var.prompt"
-		;;
-	dev)
-		echo "DEVELOPMENT" > "$DOTFILES_DIR/bash/var.env"
-		echo "COMPLEX"     > "$DOTFILES_DIR/bash/var.prompt"
-		;;
-	*)
-		#Interactive mode
-		echo "=== Welcome to installation of DotFiles server version ==="
-		echo "    Please be careful"
-		echo "!!! Warninng: some of your current home files echo will be deleted !!!"
-		read -r -n 1 -p "    Do you really want to install custom dot-files? (y/[a]): " AMSURE
-		[ "$AMSURE" = "y" ] || exit
-		echo "" 1>&2
+    # Cases for users that know what to do
+    prod)
+        echo "PRODUCTION"  > "$DOTFILES_DIR/bash/var.env"
+        echo "COMPLEX"     > "$DOTFILES_DIR/bash/var.prompt"
+        ;;
+    dev)
+        echo "DEVELOPMENT" > "$DOTFILES_DIR/bash/var.env"
+        echo "COMPLEX"     > "$DOTFILES_DIR/bash/var.prompt"
+        ;;
+    *)
+        #Interactive mode
+        echo "=== Welcome to installation of DotFiles server version ==="
+        echo "    Please be careful"
+        echo "!!! Warninng: some of your current home files echo will be deleted !!!"
+        read -r -n 1 -p "    Do you really want to install custom dot-files? (y/[a]): " AMSURE
+        [ "$AMSURE" = "y" ] || exit
+        echo "" 1>&2
 
-		PS3="   Select the name of environment for this server: "
-		ENVS=("PRODUCTION" "DEVELOPMENT" "TEST")
-		select ENVRM in "${ENVS[@]}"; do
-		echo "$ENVRM" > "$DOTFILES_DIR/bash/var.env"
-		break
-		done
+        PS3="   Select the name of environment for this server: "
+        ENVS=("PRODUCTION" "DEVELOPMENT" "TEST")
+        select ENVRM in "${ENVS[@]}"; do
+        echo "$ENVRM" > "$DOTFILES_DIR/bash/var.env"
+        break
+        done
 
-		echo ""
-		PS3="   Select complexity of bash prompt:"
-		PRPTS=("COMPLEX" "SIMPLE")
-		select PRPT in "${PRPTS[@]}"; do
-		echo "$PRPT" > "$DOTFILES_DIR/bash/var.prompt"
-		break
-		done
-		;;
+        echo ""
+        PS3="   Select complexity of bash prompt:"
+        PRPTS=("COMPLEX" "SIMPLE")
+        select PRPT in "${PRPTS[@]}"; do
+        echo "$PRPT" > "$DOTFILES_DIR/bash/var.prompt"
+        break
+        done
+        ;;
 esac
 
 mkdir -p "$HOME/.local/bin"
@@ -74,34 +74,36 @@ if [ -f /proc/version_signature ] ; then
     kernel=$(cat /proc/version_signature)
     kernelWSL="Microsoft"
     if [[ "${kernel}" =~ "${kernelWSL}" ]]; then
-    	sudo rm -rf "/etc/wsl.conf"               2> /dev/null
-    	sudo ln -sf "$DOTFILES_DIR/data/wsl.conf" "/etc/wsl.conf"
+        sudo rm -rf "/etc/wsl.conf"               2> /dev/null
+        sudo ln -sf "$DOTFILES_DIR/data/wsl.conf" "/etc/wsl.conf"
     fi
 fi
 ln -sf "$DOTFILES_DIR/bin-nix"              "$HOME/.bin"
 
 platform=`uname`
 case $platform in
-	Linux )
-		mkdir -p "$HOME/.config"
-		rm -rf "$HOME/.Xresources"               2> /dev/null
-		rm -rf "$HOME/.config/alacritty"         2> /dev/null
+    Linux )
+        mkdir -p "$HOME/.config/powershell"
+        rm -rf "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
+        rm -rf "$HOME/.Xresources"               2> /dev/null
+        rm -rf "$HOME/.config/alacritty"         2> /dev/null
 
-		ln -sf "$DOTFILES_DIR/.gitconfig-nix"    "$HOME/.gitconfig"
-		ln -sf "$DOTFILES_DIR/Xresources"        "$HOME/.Xresources"
-		ln -sf "$DOTFILES_DIR/config/alacritty"  "$HOME/.config/alacritty"
-		;;
-	Darwin )
-		mkdir -p "$HOME/.config"
-		rm -rf "$HOME/.config/alacritty"         2> /dev/null
+        ln -sf "$DOTFILES_DIR/.gitconfig-nix"    "$HOME/.gitconfig"
+        ln -sf "$DOTFILES_DIR/Xresources"        "$HOME/.Xresources"
+        ln -sf "$DOTFILES_DIR/config/alacritty"  "$HOME/.config/alacritty"
+        echo ". $DOTFILES_DIR/powershell/profile_loader.ps1" > "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
+        ;;
+    Darwin )
+        mkdir -p "$HOME/.config"
+        rm -rf "$HOME/.config/alacritty"         2> /dev/null
 
-		ln -sf "$DOTFILES_DIR/.gitconfig-nix"    "$HOME/.gitconfig"
-		ln -sf "$DOTFILES_DIR/config/alacritty"  "$HOME/.config/alacritty"
-		;;
-	MSYS_NT-10.0 )
-		ln -sf "$DOTFILES_DIR/.gitconfig-win"    "$HOME/.gitconfig"
-		ln -sf "$DOTFILES_DIR/bin-win"           "$HOME/.bin"
-		;;
+        ln -sf "$DOTFILES_DIR/.gitconfig-nix"    "$HOME/.gitconfig"
+        ln -sf "$DOTFILES_DIR/config/alacritty"  "$HOME/.config/alacritty"
+        ;;
+    MSYS_NT-10.0 )
+        ln -sf "$DOTFILES_DIR/.gitconfig-win"    "$HOME/.gitconfig"
+        ln -sf "$DOTFILES_DIR/bin-win"           "$HOME/.bin"
+        ;;
 esac
 
 touch "$HOME/.localenv"
