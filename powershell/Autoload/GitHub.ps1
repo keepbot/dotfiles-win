f24756326e9fc8263c321a9f67ad2969458fbcda
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-01-09 02:12:24 +0300

    Github batch clone cripts refactoring for powershell
    * old functions get/show_gh_user_repos were removed
    * added Github Repos command center, which combines all clonning logic in one place

--- powershell/Autoload/GitHub.ps1
+++ powershell/Autoload/GitHub.ps1
@@ -16,97 +16,154 @@ if ($MyInvocation.InvocationName -ne '.')
     Exit
 }
 
+function Set-GithubOAuthCreds
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Username,
+        [Parameter(Mandatory=$true)]
+        [string]$Token
+    )
+    [string] $SecretFile = (Join-Path $env:USERPROFILE '.github.secrets')
+    Add-Content $SecretFile "$Username"
+    Add-Content $SecretFile "$Token"
+}
 
-if (Get-Command git.exe -ErrorAction SilentlyContinue | Test-Path) {
-    # FIXME
-    ${function:get_gh_user_repos_https} = {
-        Write-Host "Clonning source GH repos of $($args[0])"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        for ($i = 1; $i -lt 10; $i++) {
-            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=1000&page=${i}" | ConvertFrom-Json
-            foreach($repo in $repoList){
-                if(-Not $repo.fork) {
-                    git.exe clone $repo.clone_url
-                }
+function Get-GithubBasicCreds
+{
+    [string] $SecretFile   = (Join-Path $env:USERPROFILE '.github.secrets')
+
+    if (-Not (Test-Path -Path $SecretFile)) {
+        Write-Host `
+            "ERROR: Secretfile $SecretFile wasn't found. Run 'Set-GitHubOAuthCreds' for initialization. Exiting..." `
+            -ForegroundColor Red
+        return
+    }
+
+    $Username   = $(Get-Content $SecretFile -First 1)
+    $Token      = $(Get-Content $SecretFile -First 2)[-1]
+
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Token)))
+    return $base64AuthInfo
+}
+
+function GithubRepos
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $GithubName,
+        [string] $Protocol = 'SSH',
+        [switch] $Organization,
+        [switch] $All,
+        [switch] $Clone
+    )
+
+    if (-Not (Get-Command git.exe -ErrorAction SilentlyContinue | Test-Path))
+    {
+        Write-Host `
+                "Error: Git not found. Please install Git for Windows and add it to PATH. Exiting..." `
+                -ForegroundColor Red
+            return
+    }
+
+    $BasicCreds = Get-GithubBasicCreds
+    $Headers = @{
+        'Authorization'=("Basic {0}" -f $BasicCreds)
+    };
+
+    if ($Organization)
+    {
+        $BaseApiUri = "https://api.github.com/orgs/${GithubName}/repos"
+    }
+    else
+    {
+        $BaseApiUri = "https://api.github.com/users/${GithubName}/repos"
+    }
+
+    if ($Clone)
+    {
+        Write-Host "Clonning Github repos of ${GithubName}"
+        $cmd = "git clone --recurse-submodules "
+    }
+    else
+    {
+        Write-Host "Showing Github repos of ${GithubName}"
+        $cmd = "Write-Output "
+    }
+
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $hasNext = $true
+    $nextUrl   = "${BaseApiUri}?sort=pushed&per_page=100"
+    # for ($i = 1; $i -lt 2; $i++)
+    while ($hasNext)
+    {
+        # Write-Output $nextUrl
+        try
+        {
+            $requestAnswer = Invoke-WebRequest -Headers $Headers -Uri $nextUrl
+        }
+        catch
+        {
+            Write-Host `
+                "Error: Github user or organization not found. Exiting..." `
+                -ForegroundColor Red
+            return
+        }
+
+        if ($requestAnswer.RelationLink["next"])
+        {
+            $nextUrl   = $requestAnswer.RelationLink["next"]
+        }
+        else
+        {
+            $hasNext = $false
+        }
+
+        $repos = $requestAnswer | ConvertFrom-Json
+        foreach($repo in $repos)
+        {
+            if((-Not $All) -And $repo.fork) { continue }
+
+            switch ($Protocol)
+            {
+                "GIT"   { Invoke-Expression ($cmd + $repo.git_url)   }
+                "HTTPS" { Invoke-Expression ($cmd + $repo.clone_url) }
+                "SSH"   { Invoke-Expression ($cmd + $repo.ssh_url)   }
+                "SVN"   { Invoke-Expression ($cmd + $repo.svn_url)   }
             }
         }
     }
+}
 
-    # FIXME
-    ${function:get_gh_user_repos_ssh} = {
-        Write-Host "Clonning source GH repos of $($args[0])"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        for ($i = 1; $i -lt 10; $i++) {
-            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=100&page=${i}" | ConvertFrom-Json
-            foreach($repo in $repoList){
-                if(-Not $repo.fork) {
-                    git.exe clone $repo.ssh_url
-                }
-            }
-        }
+function Rename-GitHub-Origin
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$NewName
+    )
+    Write-Output "Renaming repo to $NewName"
+    $dir = Get-Location
+    Get-ChildItem $dir -Directory | ForEach-Object {
+        Write-Host $_.FullName
+        Set-Location $_.FullName
+        $oldRemote = git config --get remote.origin.url
+        Write-Host "Old remote:"
+        git remote -v
+        $repo = Split-Path $oldRemote -leaf
+        $newRemote = "git@github.com:${NewName}/${repo}"
+        git remote rm origin
+        git remote add origin ${newRemote}
+        Write-Host "New remote:"
+        # Write-Host $newRemote
+        git remote -v
+        Write-Host "------------------------------------------------------------"
     }
-
-    # FIXME
-    ${function:show_gh_user_repos_ssh} = {
-        Write-Host "Showing source GH repos of $($args[0])"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        for ($i = 1; $i -lt 10; $i++) {
-            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=100&page=${i}" | ConvertFrom-Json
-            foreach($repo in $repoList){
-                if(-Not $repo.fork) {
-                    Write-Output $repo.ssh_url
-                }
-            }
-        }
-    }
-
-    # FIXME
-    ${function:get_gh_user_repos_ssh_all} = {
-        Write-Host "Clonning all GH repos of $($args[0])"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            for ($i = 1; $i -lt 10; $i++) {
-            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=100&page=${i}" | ConvertFrom-Json
-            foreach($repo in $repoList.ssh_url){
-                git.exe clone $repo
-            }
-        }
-    }
-
-    # FIXME
-    ${function:show_gh_user_repos_ssh_all} = {
-        Write-Host "Showing all GH repos of $($args[0])"
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            for ($i = 1; $i -lt 10; $i++) {
-            $repoList = Invoke-WebRequest -Uri "https://api.github.com/users/$($args[0])/repos?sort=pushed&per_page=100&page=${i}" | ConvertFrom-Json
-            foreach($repo in $repoList.ssh_url){
-                Write-Output $repo
-            }
-        }
-    }
-
-    function Rename-GitHub-Origin {
-        param
-        (
-            [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-            [string]$NewName
-        )
-        Write-Output "Renaming repo to $NewName"
-        $dir = Get-Location
-        Get-ChildItem $dir -Directory | ForEach-Object {
-            Write-Host $_.FullName
-            Set-Location $_.FullName
-            $oldRemote = git config --get remote.origin.url
-            Write-Host "Old remote:"
-            git remote -v
-            $repo = Split-Path $oldRemote -leaf
-            $newRemote = "git@github.com:${NewName}/${repo}"
-            git remote rm origin
-            git remote add origin ${newRemote}
-            Write-Host "New remote:"
-            # Write-Host $newRemote
-            git remote -v
-            Write-Host "------------------------------------------------------------"
-        }
-        Set-Location $dir
-    }
+    Set-Location $dir
 }
