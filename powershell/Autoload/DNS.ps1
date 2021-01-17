4fbac4bc17a4a0e0e233a8ab37a635eb4b71fddb
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-01-17 22:37:39 +0100

    change dig dns servers to work through cloud flare

--- powershell/Autoload/DNS.ps1
+++ powershell/Autoload/DNS.ps1
@@ -20,7 +20,9 @@ if ($MyInvocation.InvocationName -ne '.')
 if (Get-Command dig.exe -ErrorAction SilentlyContinue | Test-Path) {
     ${function:dig-init} = {
         # mkdir $env:SystemRoot\System32\Drivers\etc\
-        Write-Output 'nameserver 8.8.8.8' > $env:SystemRoot\System32\Drivers\etc\resolv.conf
-        Write-Output 'nameserver 77.88.8.8' >> $env:SystemRoot\System32\Drivers\etc\resolv.conf
+        # Write-Output 'nameserver 8.8.8.8' > $env:SystemRoot\System32\Drivers\etc\resolv.conf
+        # Write-Output 'nameserver 77.88.8.8' >> $env:SystemRoot\System32\Drivers\etc\resolv.conf
+        Write-Output 'nameserver 1.1.1.1' > $env:SystemRoot\System32\Drivers\etc\resolv.conf
+        Write-Output 'nameserver 1.0.0.1' > $env:SystemRoot\System32\Drivers\etc\resolv.conf
     }
 }
