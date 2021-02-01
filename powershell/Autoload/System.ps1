691e89305ec9e996d37fd64a1854d4784c447ead
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-02-01 09:58:03 +0100

    add function to enable Different Input Methods for each App

--- powershell/Autoload/System.ps1
+++ powershell/Autoload/System.ps1
@@ -218,3 +218,13 @@ function Get-SystemInfo
 {
     Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property *
 }
+
+Function EnableDifferentInputMethodForEachApp
+{
+    $prefMask = (Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name 'UserPreferencesMask').UserPreferencesMask
+    if (($prefMask[4] -band 0x80) -eq 0)
+    {
+        $prefMask[4] = ($prefMask[4] -bor 0x80)
+        New-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name 'UserPreferencesMask' -Value $prefMask -PropertyType ([Microsoft.Win32.RegistryValueKind]::Binary) -Force | Out-Null
+    }
+}
