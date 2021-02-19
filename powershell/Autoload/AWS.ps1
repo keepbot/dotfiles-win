f5a22b43e35b0932bebdb378aa8bfda9199c7bd4
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-02-19 01:09:10 +0100

    add aws functions to init mfa session and assume role

--- powershell/Autoload/AWS.ps1
+++ powershell/Autoload/AWS.ps1
@@ -66,3 +66,42 @@ function aws_print_all_ip()
         }
     }
 }
+
+function aws_set_mfa_session()
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $MFADevice,
+        [Parameter(Mandatory=$true)]
+        [string] $MFAToken,
+        [string] $Duration = 1800,
+        [string] $Region = 'eu-west-1'
+    )
+    $creds = ((aws sts get-session-token --serial-number $MFADevice --token-code $MFAToken --duration-seconds $Duration) | ConvertFrom-Json).Credentials
+
+    Set-Item -Path Env:AWS_ACCESS_KEY_ID -Value $creds.AccessKeyId
+    Set-Item -Path Env:AWS_SECRET_ACCESS_KEY -Value $creds.SecretAccessKey
+    Set-Item -Path Env:AWS_SESSION_TOKEN -Value $creds.SessionToken
+    Set-Item -Path Env:AWS_REGION -Value $Region
+}
+
+function aws_assume_role()
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $RoleARN,
+        [Parameter(Mandatory=$true)]
+        [string] $SessionName,
+        [string] $Region = 'eu-west-1'
+    )
+    $creds = ((aws sts assume-role --role-arn ${RoleARN} --role-session-name=${SessionName}) | ConvertFrom-Json).Credentials
+
+    Set-Item -Path Env:AWS_ACCESS_KEY_ID -Value $creds.AccessKeyId
+    Set-Item -Path Env:AWS_SECRET_ACCESS_KEY -Value $creds.SecretAccessKey
+    Set-Item -Path Env:AWS_SESSION_TOKEN -Value $creds.SessionToken
+    Set-Item -Path Env:AWS_REGION -Value $Region
+}
