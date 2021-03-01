e03191935705ee5dcf23a978bc6c681c0c78de0e
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-03-02 00:01:51 +0100

    add WebAssembly Binary Toolkit

--- powershell/Autoload/Environment.ps1
+++ powershell/Autoload/Environment.ps1
@@ -231,6 +231,7 @@ function Initialize-Paths-User
         "C:\tools\swig"
         "C:\tools\vcpkg"
         "C:\tools\vim\vim82"
+        "C:\tools\wabt\1.0.20"
         "C:\tools\wsl\arch"
         "C:\tools\wsl\debian"
         "C:\tools\wsl\kali"
