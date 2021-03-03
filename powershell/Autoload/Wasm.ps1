982f48cbbeebfc1736dc90662aedc33c4ec5d1f6
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-03-03 12:08:58 +0100

    add switch button to enable or diable emcc debug

--- powershell/Autoload/Wasm.ps1
+++ powershell/Autoload/Wasm.ps1
@@ -17,3 +17,31 @@ if ($MyInvocation.InvocationName -ne '.')
 }
 
 ${function:wasm_build} = { emcc.py -o a.out.html -s STANDALONE_WASM=1 -s WASM_BIGINT=1 -O3 -v @args }
+
+function wasm_debug
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Button
+    )
+
+    switch ($Button)
+    {
+        ({$PSItem -eq 'On' -Or $PSItem -eq 'on'})
+        {
+            Set-Item -Path Env:EMCC_DEBUG -Value 1
+            break
+        }
+        ({$PSItem -eq 'Off' -Or $PSItem -eq 'off'})
+        {
+            Set-Item -Path Env:EMCC_DEBUG -Value 0
+            break
+        }
+        default
+        {
+            Write-Host "ERROR: Wrong operation..." -ForegroundColor Red
+            Write-Host "  Usage: wasm_debug <On|Off>" -ForegroundColor Red
+        }
+    }
+}
