ca4136226e9928f310cdabaf086b75325316115c
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-03-26 00:43:08 +0100

    rename functions for fixing anyconnect-related network issues on windows and wsl

--- powershell/Autoload/WSL.ps1
+++ powershell/Autoload/WSL.ps1
@@ -56,7 +56,7 @@ function List-WSLDistros()
     wsl.exe --list -v
 }
 
-function FixWSLInternetWithCisco()
+function anyconnect-wsl-fix()
 {
     Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Set-NetIPInterface -InterfaceMetric 4000
     Get-NetIPInterface -InterfaceAlias "vEthernet (WSL)" | Set-NetIPInterface -InterfaceMetric 1
