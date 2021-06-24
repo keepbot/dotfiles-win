83b7dc9c1fda68b2be2e07e58af0fc79f6958b2c
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-06-24 15:06:14 +0200

    add new kubernetes aliases

--- powershell/Autoload/Kubernestes.ps1
+++ powershell/Autoload/Kubernestes.ps1
@@ -15,10 +15,18 @@ if ($MyInvocation.InvocationName -ne '.')
     Exit
 }
 
-${function:kdev}   = { kubectl --context dev     @args }
-${function:kstage} = { kubectl --context stage   @args }
-${function:kprod}  = { kubectl --context prod    @args }
+${function:kdev}        = { kubectl --context dev       @args }
+${function:kstage}      = { kubectl --context stage     @args }
+${function:kprod}       = { kubectl --context prod      @args }
 
-${function:hdev}   = { helm --kube-context dev   @args }
-${function:hstage} = { helm --kube-context stage @args }
-${function:hprod}  = { helm --kube-context prod  @args }
+${function:kdev_cn}     = { kubectl --context devch     @args }
+${function:kstage_cn}   = { kubectl --context stagech   @args }
+${function:kprod_cn}    = { kubectl --context prodch    @args }
+
+${function:hdev}        = { helm --kube-context dev     @args }
+${function:hstage}      = { helm --kube-context stage   @args }
+${function:hprod}       = { helm --kube-context prod    @args }
+
+${function:hdev_cn}     = { helm --kube-context devch   @args }
+${function:hstage_cn}   = { helm --kube-context stagech @args }
+${function:hprod_cn}    = { helm --kube-context prodch  @args }
