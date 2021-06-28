7bc761c83b9726b20aad7393420acf14fed63893
Dmitriy Ivanov <d.k.ivanov@live.com>
2021-06-28 13:27:24 +0200

    add new kubernetes aliases: retrieving tokens

--- bash/autoload/kubernetes.sh
+++ bash/autoload/kubernetes.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-alias   kdev='kubectl --context dev'
-alias kstage='kubectl --context stage'
-alias  kprod='kubectl --context prod'
+alias      kdev="kubectl --context dev"
+alias    kstage="kubectl --context stage"
+alias     kprod="kubectl --context prod"
+alias   kdev_cn="kubectl --context devch"
+alias kstage_cn="kubectl --context stagech"
+alias  kprod_cn="kubectl --context prodch"
 
-alias   hdev='helm --kube-context dev'
-alias hstage='helm --kube-context stage'
-alias  hprod='helm --kube-context prod'
+alias      hdev="helm --kube-context dev"
+alias    hstage="helm --kube-context stage"
+alias     hprod="helm --kube-context prod"
+alias   hdev_cn="helm --kube-context devch"
+alias hstage_cn="helm --kube-context stagech"
+alias  hprod_cn="helm --kube-context prodch"
+
+alias      kdev_token="kubectl --context dev     -n kube-system describe secret $(kubectl -n kube-system get secret | grep service-controller-token | awk '{print $1}')"
+alias    kstage_token="kubectl --context stage   -n kube-system describe secret $(kubectl -n kube-system get secret | grep service-controller-token | awk '{print $1}')"
+alias     kprod_token="kubectl --context prod    -n kube-system describe secret $(kubectl -n kube-system get secret | grep service-controller-token | awk '{print $1}')"
+alias   kdev_cn_token="kubectl --context devch   -n kube-system describe secret $(kubectl -n kube-system get secret | grep service-controller-token | awk '{print $1}')"
+alias kstage_cn_token="kubectl --context stagech -n kube-system describe secret $(kubectl -n kube-system get secret | grep service-controller-token | awk '{print $1}')"
+alias  kprod_cn_token="kubectl --context prodch  -n kube-system describe secret $(kubectl -n kube-system get secret | grep service-controller-token | awk '{print $1}')"
--- powershell/Autoload/Kubernestes.ps1
+++ powershell/Autoload/Kubernestes.ps1
@@ -15,18 +15,59 @@ if ($MyInvocation.InvocationName -ne '.')
     Exit
 }
 
-${function:kdev}        = { kubectl --context dev       @args }
-${function:kstage}      = { kubectl --context stage     @args }
-${function:kprod}       = { kubectl --context prod      @args }
+${function:kdev}            = { kubectl --context dev       @args }
+${function:kstage}          = { kubectl --context stage     @args }
+${function:kprod}           = { kubectl --context prod      @args }
+${function:kdev_cn}         = { kubectl --context devch     @args }
+${function:kstage_cn}       = { kubectl --context stagech   @args }
+${function:kprod_cn}        = { kubectl --context prodch    @args }
 
-${function:kdev_cn}     = { kubectl --context devch     @args }
-${function:kstage_cn}   = { kubectl --context stagech   @args }
-${function:kprod_cn}    = { kubectl --context prodch    @args }
+${function:hdev}            = { helm --kube-context dev     @args }
+${function:hstage}          = { helm --kube-context stage   @args }
+${function:hprod}           = { helm --kube-context prod    @args }
+${function:hdev_cn}         = { helm --kube-context devch   @args }
+${function:hstage_cn}       = { helm --kube-context stagech @args }
+${function:hprod_cn}        = { helm --kube-context prodch  @args }
 
-${function:hdev}        = { helm --kube-context dev     @args }
-${function:hstage}      = { helm --kube-context stage   @args }
-${function:hprod}       = { helm --kube-context prod    @args }
-
-${function:hdev_cn}     = { helm --kube-context devch   @args }
-${function:hstage_cn}   = { helm --kube-context stagech @args }
-${function:hprod_cn}    = { helm --kube-context prodch  @args }
+function kdev_token()
+{
+    kubectl --context dev -n kube-system get secret `
+    $((kubectl --context prodch -o yaml -n kube-system get serviceaccounts service-account-controller `
+    | ConvertFrom-Yaml).secrets.name) -o jsonpath="{.data.token}" `
+    | ConvertFrom-Base64
+}
+function kstage_token()
+{
+    kubectl --context stage -n kube-system get secret `
+    $((kubectl --context prodch -o yaml -n kube-system get serviceaccounts service-account-controller `
+    | ConvertFrom-Yaml).secrets.name) -o jsonpath="{.data.token}" `
+    | ConvertFrom-Base64
+}
+function kprod_token()
+{
+    kubectl --context prod -n kube-system get secret `
+    $((kubectl --context prodch -o yaml -n kube-system get serviceaccounts service-account-controller `
+    | ConvertFrom-Yaml).secrets.name) -o jsonpath="{.data.token}" `
+    | ConvertFrom-Base64
+}
+function kdev_cn_token()
+{
+    kubectl --context devch -n kube-system get secret `
+    $((kubectl --context prodch -o yaml -n kube-system get serviceaccounts service-account-controller `
+    | ConvertFrom-Yaml).secrets.name) -o jsonpath="{.data.token}" `
+    | ConvertFrom-Base64
+}
+function kstage_cn_token()
+{
+    kubectl --context stagech -n kube-system get secret `
+    $((kubectl --context prodch -o yaml -n kube-system get serviceaccounts service-account-controller `
+    | ConvertFrom-Yaml).secrets.name) -o jsonpath="{.data.token}" `
+    | ConvertFrom-Base64
+}
+function kprod_cn_token()
+{
+    kubectl --context prodch -n kube-system get secret `
+    $((kubectl --context prodch -o yaml -n kube-system get serviceaccounts service-account-controller `
+    | ConvertFrom-Yaml).secrets.name) -o jsonpath="{.data.token}" `
+    | ConvertFrom-Base64
+}
